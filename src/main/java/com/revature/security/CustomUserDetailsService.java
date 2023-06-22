cf3b42dd090fa781a50e1a73f1ae4f136ac04a36
DeShon Dixon <deshondixon@gmail.com>
2023-06-21 21:27:28 -0700

    skeleton

--- src/main/java/com/revature/security/CustomUserDetailsService.java
+++ src/main/java/com/revature/security/CustomUserDetailsService.java
@@ -1,5 +1,8 @@
 package com.revature.security;
 
+import com.revature.daos.EmployeeDAO;
+import com.revature.models.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,8 +11,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
+    private final EmployeeDAO employeeDAO;
+
+    @Autowired
+    public CustomUserDetailsService(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        Employee e = employeeDAO.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("No Employee found"));
     }
 }
