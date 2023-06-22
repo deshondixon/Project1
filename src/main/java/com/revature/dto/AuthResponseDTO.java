841414b94f5642677f86ba9fd03153bb73a4a64a
DeShon Dixon <deshondixon@gmail.com>
2023-06-22 16:03:03 -0700

    acp

--- src/main/java/com/revature/dto/AuthResponseDTO.java
+++ src/main/java/com/revature/dto/AuthResponseDTO.java
@@ -1,34 +1,16 @@
 package com.revature.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class AuthResponseDTO {
+
     private String accessToken;
+
     private String tokenType = "Bearer ";
 
-    public AuthResponseDTO(String accessToken) {
-        this.accessToken = accessToken;
-    }
-
-    public String getAccessToken() {
-        return accessToken;
-    }
-
-    public void setAccessToken(String accessToken) {
-        this.accessToken = accessToken;
-    }
-
-    public String getTokenType() {
-        return tokenType;
-    }
-
-    public void setTokenType(String tokenType) {
-        this.tokenType = tokenType;
-    }
-
-    @Override
-    public String toString() {
-        return "AuthResponseDTO{" +
-                "accessToken='" + accessToken + '\'' +
-                ", tokenType='" + tokenType + '\'' +
-                '}';
-    }
 }
