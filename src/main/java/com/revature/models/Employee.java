3d2c73e9b9eb814aca26add9e9898037664d236a
DeShon Dixon <deshondixon@gmail.com>
2023-06-22 15:58:51 -0700

    acp

--- src/main/java/com/revature/models/Employee.java
+++ src/main/java/com/revature/models/Employee.java
@@ -1,10 +1,17 @@
 package com.revature.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
 @Table(name= "employees")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Employee {
 
     @Id
@@ -31,85 +38,5 @@ public class Employee {
     @ManyToMany
     private List<Reimbursement> reimbursements;
 
-    public Employee() {
-    }
-
-    public Employee(String firstName, String lastName, Position position, String username, String password, List<Reimbursement> reimbursements) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.position = position;
-        this.username = username;
-        this.password = password;
-        this.reimbursements = reimbursements;
-    }
-
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Position getPosition() {
-        return position;
-    }
-
-    public void setPosition(Position position) {
-        this.position = position;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public List<Reimbursement> getReimbursements() {
-        return reimbursements;
-    }
-
-    public void setReimbursements(List<Reimbursement> reimbursements) {
-        this.reimbursements = reimbursements;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", position=" + position +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", reimbursements=" + reimbursements +
-                '}';
-    }
+//
 }
--- src/main/java/com/revature/models/Position.java
+++ src/main/java/com/revature/models/Position.java
@@ -1,9 +1,16 @@
 package com.revature.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name="positions")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Position {
 
     @Id
@@ -14,35 +21,4 @@ public class Position {
     @Column(unique = true)
     private String name;
 
-    public Position() {
-    }
-
-    public Position(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Position{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
 }
