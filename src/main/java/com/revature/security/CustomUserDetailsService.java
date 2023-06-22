126aafc464f03e7549ac3c831a2b63c5eb457300
DeShon Dixon <deshondixon@gmail.com>
2023-06-21 21:37:25 -0700

    setup

--- src/main/java/com/revature/security/CustomUserDetailsService.java
+++ src/main/java/com/revature/security/CustomUserDetailsService.java
@@ -2,11 +2,19 @@ package com.revature.security;
 
 import com.revature.daos.EmployeeDAO;
 import com.revature.models.Employee;
+import com.revature.models.Position;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -22,5 +30,12 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Employee e = employeeDAO.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("No Employee found"));
+        return new User(e.getUsername(), e.getPassword(), mapPositionToAuthority(e.getPosition()));
+    }
+
+    private Collection<GrantedAuthority> mapPositionToAuthority(Position position){
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority(position.getName()));
+        return grantedAuthorities;
     }
 }
