bb824a3979f2199022f18c468dbe179d25ec2104
DeShon Dixon <deshondixon@gmail.com>
2023-06-22 18:08:21 -0700

    acp

--- src/main/java/com/revature/controllers/EmployeeController.java
+++ src/main/java/com/revature/controllers/EmployeeController.java
@@ -61,24 +61,6 @@ public class EmployeeController {
         }
     }
 
-    public Employee registerForReimbursement(int employeeId, int reimbursementId) {
-        Employee employee = getEmployeeById(employeeId);
-        Reimbursement reimbursement = reimbursementService.findReimbursementById(reimbursementId);
-
-        List<Reimbursement> reimbursements = employee.getReimbursements();
-
-        if (!reimbursements.contains(reimbursement)) {
-            reimbursements.add(reimbursement);
-            employee.setReimbursements(reimbursements);
-            employeeDAO.save(employee);
-        }
-
-        return employee;
-    }
-
-    private Employee getEmployeeById(int employeeId) {
-    }
-
 
     // unregister
     @DeleteMapping("{eid}/reimbursements/unregister/{rid}")
