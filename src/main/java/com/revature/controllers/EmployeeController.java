86c8b7a22cfebf541c6886f4ea13fc34adde1b09
DeShon Dixon <deshondixon@gmail.com>
2023-06-28 11:52:40 -0700

    acp

--- src/main/java/com/revature/controllers/EmployeeController.java
+++ src/main/java/com/revature/controllers/EmployeeController.java
@@ -1,3 +1,76 @@
+//package com.revature.controllers;
+//
+//import com.revature.models.Employee;
+//import com.revature.models.Reimbursement;
+//import com.revature.security.JwtGenerator;
+//import com.revature.services.EmployeeService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("employees")
+//public class EmployeeController {
+//    private final EmployeeService employeeService;
+//
+//
+//    private final JwtGenerator jwtGenerator;
+//
+//    @Autowired
+//    public EmployeeController(EmployeeService employeeService, JwtGenerator jwtGenerator) { this.employeeService = employeeService;
+//        this.jwtGenerator = jwtGenerator;
+//    }
+//
+//    //GET-ALL
+//    @GetMapping
+//    public List<Employee> getAllEmployeeHandler(){ return employeeService.getAllEmployee();}
+//
+//    //INSERT
+//    @PostMapping
+//    public Employee createEmployeeHandler(@RequestBody Employee emp) { return employeeService.createEmployee(emp);}
+//
+//    //UPDATE
+//    @PutMapping
+//    public Employee updateEmployeeHandler(@RequestBody Employee emp) { return employeeService.updateEmployee(emp);}
+//
+//    //DELETE
+//    @DeleteMapping("{id}")
+//    public boolean deleteEmployeeHandler(@PathVariable("id") int id){
+//        return employeeService.deleteEmployeeById(id);
+//    }
+//
+//
+//    @GetMapping("{id}/reimbursements")
+//    public List<Reimbursement> getReimbursementsFromReimbursementHandler(@PathVariable("id") int id){
+//        return employeeService.getReimbursementByEmployeeId(id);
+//    }
+//
+//    // register
+//    @PostMapping("{eid}/reimbursements/register/{rid}")
+//    public ResponseEntity<?> registerForReimbursementHandler(@PathVariable("eid") int eid, @PathVariable("rid") int rid, @RequestHeader("Authorization") String bearerToken ){
+//        String username = jwtGenerator.getUsernameFromToken(bearerToken.substring(7));
+//        Employee e = employeeService.findEmployeeByUsername(username);
+//
+//        if (e.getId() == eid){
+//            return new ResponseEntity<Employee>(employeeService.registerForReimbursement(eid, rid), HttpStatus.OK);
+//        } else {
+//            return new ResponseEntity<String>("Cannot access another Employees Information!",HttpStatus.FORBIDDEN);
+//        }
+//    }
+//
+//
+//    // unregister
+//    @DeleteMapping("{eid}/reimbursements/unregister/{rid}")
+//    public Employee unregisterForReimbursementHandler(@PathVariable("eid") int eid, @PathVariable("rid") int rid){
+//        return employeeService.unregisterForReimbursement(eid, rid);
+//    }
+//
+//
+//}
+
 package com.revature.controllers;
 
 import com.revature.models.Employee;
@@ -44,21 +117,14 @@ public class EmployeeController {
 
     // INSERT
     @PostMapping
-    public ResponseEntity<?> createEmployeeHandler(@RequestBody Employee employee) {
-        String username = employee.getUsername();
-
-        if (usernameIsRegistered(username)) {
-            return new ResponseEntity<>("Username is already registered!", HttpStatus.CONFLICT);
-        }
-
-        Employee createdEmployee = employeeService.createEmployee(employee);
-        return new ResponseEntity<>(createdEmployee, HttpStatus.OK);
+    public Employee createEmployeeHandler(@RequestBody Employee emp) {
+        return employeeService.createEmployee(emp);
     }
 
     // UPDATE
     @PutMapping
-    public Employee updateEmployeeHandler(@RequestBody Employee employee) {
-        return employeeService.updateEmployee(employee);
+    public Employee updateEmployeeHandler(@RequestBody Employee emp) {
+        return employeeService.updateEmployee(emp);
     }
 
     // DELETE
@@ -84,11 +150,6 @@ public class EmployeeController {
         } else {
             return new ResponseEntity<>("Cannot create reimbursement for another employee.", HttpStatus.FORBIDDEN);
         }
-    }
-
-    private boolean usernameIsRegistered(String username) {
-        Employee employee = employeeService.findEmployeeByUsername(username);
-        return employee != null;
     }
 
 
